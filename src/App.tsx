d963852fa2d879266417f0db1c83d3206f3927be
Suzanne Cabral <suzannecabral@live.com>
2021-03-23 14:05:14 -0700

    Added sass, favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import './App.scss';
 
 interface AppProps {}
 
 function App({}: AppProps) {
+  console.log("app is loaded");
   // states go here
   // useEffects go here
   // Return the App component.
   return (
     <div className="App">
       <h1>Snowpack Typescript App!</h1>
+      <p>this is a paragraph with some text. I'm writing a lot of text so that maybe it will line wrap and look like a paragraph.</p>
     </div>
   );
 }
